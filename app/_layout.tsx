de0c6cf4ff8ea6d88999df156aea820b92c71795
mhlehri <lehrimirza101@gmail.com>
2025-06-14 15:49:46 +0600

    practice project init...

---
+++ app/_layout.tsx
@@ -0,0 +1,14 @@
+import { Stack } from "expo-router";
+import "./global.css";
+
+export default function RootLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      {/* <Stack.Screen /> */}
+    </Stack>
+  );
+}
