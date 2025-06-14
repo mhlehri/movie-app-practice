83faa6c42b3724e0d54fdec2195c0726a0586cae
mhlehri <lehrimirza101@gmail.com>
2025-06-14 16:41:06 +0600

    create searchBar component and create fetchMovies api service

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,14 @@
+import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { useRouter } from "expo-router";
 import React from "react";
 import { Image, ScrollView, View } from "react-native";
 
-const index = () => {
+export default function Index() {
+  const router = useRouter();
+
+
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0"/>
@@ -16,9 +21,14 @@ const index = () => {
         }}
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
+      <View className="flex-1 mt-5">
+        <SearchBar 
+          onPress={() => router.push("/search")}
+          placeholder="Search for a movie"
+        />
+      </View> 
       </ScrollView>
     </View>
   );
 };
 
-export default index;
